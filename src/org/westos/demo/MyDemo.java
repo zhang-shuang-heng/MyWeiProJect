ea27f057a44751cbc24941c391707f53bcacbe07
zxcvbn <sublun@126.com>
2019-09-11 17:25:47 +0800

    Initial commit

---
+++ src/org/westos/demo/MyDemo.java
@@ -0,0 +1,22 @@
+package org.westos.demo;
+
+/**
+ * @Author: ShenMouMou
+ * @CreateTime: 2019-09-11 13:46
+ * @Description:西部开源教育科技有限公司
+ */
+public class MyDemo {
+    public static void main(String[] args) {
+        System.out.println("我编写了第一个功能");
+        System.out.println("我编写了第二个功能");
+		System.out.println("zhangsan我编写了第二个功能");
+		System.out.println("zhangsan我编写了第555555555555555555555个功能");
+        System.out.println("lsisi4444444444444444我编写了第二个功能");
+        System.out.println("改代码");
+        System.out.println("我是xxx");
+        System.out.println("改掉zaj");
+        System.out.println("再改掉zaj");
+        System.out.println("dadadda");
+        System.out.println("zaj有本事改我呀");
+    }
+}
---
+++ src/org/westos/demo/MyUtils.java
@@ -0,0 +1,12 @@
+package org.westos.demo;
+
+/**
+ * @Author: ShenMouMou
+ * @CreateTime: 2019-09-11 13:58
+ * @Description:西部开源教育科技有限公司
+ */
+public class MyUtils {
+    public static void add(){
+        System.out.println("这是一个相加的方法");
+    }
+}
